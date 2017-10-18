a78ca3011a0af93fbc38354bfa079897b2b84905
Maksim Kholod <maksi123p@gmail.com>
2017-10-18 18:42:45 +0300

    install symfony/routing

--- web/front.php
+++ web/front.php
@@ -1,24 +1,25 @@
 <?php
 
-require_once __DIR__.'/vendor/autoload.php';
+require_once __DIR__.'/../vendor/autoload.php';
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 $request = Request::createFromGlobals();
-$response = new Response();
 
 $map = array(
-    '/hello' => __DIR__.'/hello.php',
-    '/bye'   => __DIR__.'/bye.php',
+    '/hello' => 'hello',
+    '/bye'   => 'bye',
 );
 
 $path = $request->getPathInfo();
 if (isset($map[$path])) {
-    require $map[$path];
+    ob_start();
+    extract($request->query->all(), EXTR_SKIP);
+    include sprintf(__DIR__.'/../src/pages/%s.php', $map[$path]);
+    $response = new Response(ob_get_clean());
 } else {
-    $response->setStatusCode(404);
-    $response->setContent('Not Found');
+    $response = new Response('Not Found', 404);
 }
 
 $response->send();
